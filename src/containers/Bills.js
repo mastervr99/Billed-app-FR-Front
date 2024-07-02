946bc1005ba41c22517858781b66320bc2eb2571
mastervr99 <web.dev418@gmail.com>
2024-07-02 10:57:35 +0200

    [Bug report] - Bills - WIP

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -33,7 +33,7 @@ export default class {
       .bills()
       .list()
       .then(snapshot => {
-        const bills = snapshot
+        let bills = snapshot
           .map(doc => {
             try {
               return {
@@ -53,6 +53,10 @@ export default class {
             }
           })
           console.log('length', bills.length)
+          
+          bills = bills.sort((a, b) => new Date(a.date) - new Date(b.date) || a.id.localeCompare(b.id))
+          console.log('bills', bills)
+
         return bills
       })
     }
