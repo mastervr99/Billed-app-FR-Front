1cf7c3f3a7f584f4ecbe3fda887d45d148ec3b8d
mastervr99 <web.dev418@gmail.com>
2024-07-09 12:31:10 +0200

    tests : it should render new bill page ok

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -7,8 +7,10 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
+import userEvent from '@testing-library/user-event'
+import Bills from "../containers/Bills.js";
 import router from "../app/Router.js";
+import NewBillUI from "../views/NewBillUI.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -36,5 +38,41 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+
   })
+    
+  //******************************************************** */
+  describe('When I am on Bills Page and I click on the button to add a bill', () => {
+    test("It should renders New Bill page", async () => {
+
+      
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      }
+      
+      const store = jest.fn();
+      
+      const displayed_bills = new Bills({document, onNavigate, store, localStorageMock})
+      
+      document.body.innerHTML = BillsUI({ data: bills });
+
+      const createNewBillButton =  screen.getByTestId("btn-new-bill");
+
+      const handleClickNewBill = jest.fn((e) => displayed_bills.handleClickNewBill);
+
+      createNewBillButton.addEventListener('click', handleClickNewBill);
+
+      userEvent.click(createNewBillButton);
+
+      expect(handleClickNewBill).toHaveBeenCalled();
+
+      document.body.innerHTML = NewBillUI();
+
+      await waitFor(() => screen.getAllByText("Envoyer une note de frais") );
+
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
+    });
+  })
+
+
 })
